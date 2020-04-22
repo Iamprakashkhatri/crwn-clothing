46b7528c30bad815bbe27cc944a6d1eeab0d6bde
prakash <prakash>
2020-04-22 12:18:28 +0545

    update our app with a shop page,collection preview component and collection-item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -9,27 +9,32 @@ class Directory extends React.Component {
             sections:[{
                 title:'Hats',
                 imageUrl:'https://i.ibb.co/cvpntL1/hats.png',
+                linkUrl:'hats',
                 id:1
             },
             {
                 title:'Jackets',
                 imageUrl:'https://i.ibb.co/px2tCc3/jackets.png',
+                linkUrl:'',
                 id:2
             },
             {
                title:'Sneakers',
                imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png',
+               linkUrl:'',
                id:3 
             },
             {
                 title:'Womens',
                 imageUrl:'https://i.ibb.co/GCCdy8t/womens.png',
+                linkUrl:'',
                 size:'large',
                 id:4
             },
             {
                 title:'Mens',
                 imageUrl:'https://i.ibb.co/R70vBrQ/men.png',
+                linkUrl:'',
                 size:'large',
                 id:5
             }
@@ -40,8 +45,8 @@ class Directory extends React.Component {
     render(){
         return(
             <div className='directory-menu'>
-                {this.state.sections.map(({title,imageUrl,id,size})=>(
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({id,...otherSectionProps})=>(
+                    <MenuItem key={id}{...otherSectionProps} />
                 ))}
             </div>
         )
